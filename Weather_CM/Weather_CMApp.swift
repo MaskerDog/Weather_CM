a5cc78cad984b0f8b75c980bd87c0b18beaed160
masker_dog <masukawa@jec.ac.jp>
2022-01-05 09:59:47 +0900

    first commit

---
+++ Weather_CM/Weather_CMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_CMApp.swift
+//  Weather_CM
+//
+//  Created by npc on 2022/01/05.
+//
+
+import SwiftUI
+
+@main
+struct Weather_CMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
