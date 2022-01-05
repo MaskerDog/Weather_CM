a4a409b95973452292cfbd1124b1959a6aa18d6c
masker_dog <masukawa@jec.ac.jp>
2022-01-05 10:44:48 +0900

    アプリ用の定数（appidは自分のものに変更する)

---
+++ Weather_CM/AppProperties.swift
@@ -0,0 +1,14 @@
+//
+//  AppProperties.swift
+//  Weather_CM
+//
+//  Created by npc on 2022/01/05.
+//
+
+import Foundation
+
+let openWeatherURL: String = "https://api.openweathermap.org/data/2.5/"
+let oneCall: String = "onecall"
+
+/// API keyは自分のものに変更する
+let appid: String = ""
---
+++ Weather_CM/Model/HTTPCommunication.swift
@@ -0,0 +1,8 @@
+//
+//  HTTPCommunication.swift
+//  Weather_CM
+//
+//  Created by npc on 2022/01/05.
+//
+
+import Foundation
